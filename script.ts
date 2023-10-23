874e151350e6f866fb4918f735e8b10e37d5a301
D. Kushnir <student95tnu@gmail.com>
2023-10-23 12:08:51 +0300

    Filtering forwarded posts

--- script.ts
+++ script.ts
@@ -1,3 +1,10 @@
+type Data = {
+  data: {
+    postNumber: number
+    forwarded: boolean
+  }
+};
+
 const hidePostsElement = document.getElementById('hidePosts');
 
 hidePostsElement?.addEventListener('click', async () => await handleEvent());
@@ -18,16 +25,20 @@ chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
   }
 });
 
-chrome.storage.sync.get('postNumber').then(({ postNumber }) => {
+chrome.storage.sync.get('data').then(({ data }: Data) => {
   const input = document.querySelector<HTMLInputElement>('#postnumber');
   if (input) {
-    input.value = postNumber;
+    input.value = data.postNumber.toString();
   }
 });
 
 const handleEvent = async () => {
   const postNumber = document.querySelector<HTMLInputElement>('#postnumber');
-  chrome.storage.sync.set({ postNumber: parseInt(postNumber?.value || '0') });
+  const forwarded =
+    !!document.querySelector<HTMLInputElement>('#hide_forwarded')?.checked;
+  chrome.storage.sync.set({
+    postNumber: parseInt(postNumber?.value || '0'), forwarded
+  });
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const tabId = tab?.id;
 
@@ -43,14 +54,14 @@ const handleEvent = async () => {
 };
 
 const hidePostsBoard = () => {
-  chrome.storage.sync.get('postNumber').then(({ postNumber }) => {
+  chrome.storage.sync.get('data').then(({ data }: Data) => {
     document.querySelectorAll('div.post').forEach(element => {
 
       const repliesSection = element.getElementsByClassName('post__refmap')[0];
       const replies = repliesSection?.getElementsByClassName('post-reply-link');
       const hiddenClass = 'post_type_hidden';
 
-      if (postNumber <= (replies?.length || 0)) {
+      if (data.postNumber <= (replies?.length || 0)) {
         element.classList.remove(hiddenClass);
       } else {
         element.classList.add(hiddenClass);
@@ -60,14 +71,14 @@ const hidePostsBoard = () => {
 };
 
 const hidePosts4Board = () => {
-  chrome.storage.sync.get('postNumber').then(({ postNumber }) => {
+  chrome.storage.sync.get('data').then(({ data }: Data) => {
     document.querySelectorAll('div.postContainer').forEach(element => {
 
       const repliesSection = element.getElementsByClassName('backlink')[0];
       const replies = repliesSection?.getElementsByClassName('quotelink');
       const hiddenClass = 'post-hidden';
 
-      if (postNumber <= (replies?.length || 0)) {
+      if (data.postNumber <= (replies?.length || 0)) {
         element.classList.remove(hiddenClass);
       } else {
         element.classList.add(hiddenClass);
@@ -77,8 +88,9 @@ const hidePosts4Board = () => {
 };
 
 const hidePostsTelegram = () => {
-  chrome.storage.sync.get('postNumber').then(({ postNumber }) => {
+  chrome.storage.sync.get('data').then(({ data }: Data) => {
     document.querySelectorAll('div.channel-post').forEach(element => {
+      const isForwared = !!element.getElementsByClassName('peer-title').length;
       const getReactionNumber = (text: string) => parseFloat(text) *
         (text.includes('K') ? 1000 : 1);
 
@@ -89,7 +101,7 @@ const hidePostsTelegram = () => {
         0
       );
 
-      if (count < postNumber) {
+      if ((data.forwarded && isForwared) || count < data.postNumber) {
         element.setAttribute('hidden', '');
       } else {
         element.removeAttribute('hidden');
@@ -99,7 +111,7 @@ const hidePostsTelegram = () => {
 };
 
 const hideVideosYoutube = () => {
-  chrome.storage.sync.get('postNumber').then(({ postNumber }) => {
+  chrome.storage.sync.get('data').then(({ data }: Data) => {
     document.querySelectorAll('div.ytd-rich-item-renderer').forEach(element => {
       const viewCountData =
         element.querySelectorAll('span.ytd-video-meta-block')[0]?.
@@ -110,7 +122,7 @@ const hideVideosYoutube = () => {
         const order = viewCountData[1]?.includes('.')
           ? 10 ** 3 : viewCountData[1]?.includes(' ')
             ? 10 ** 6 : 1; // looking for thousands and millions
-        if (viewNumber * order < postNumber) {
+        if (viewNumber * order < data.postNumber) {
           element.setAttribute('hidden', '');
         } else {
           element.removeAttribute('hidden');
